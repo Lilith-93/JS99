3da492a7782c5d7f8ec0b86ee05abf995afbf7fb
Lilith-93 <deborah.delcourt@free.fr>
2017-07-24 23:36:54 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,7 @@ function count(){
 //On crée la variable nbclick
 	var nbclick = 0;
 //A chaque click, on augmente nbclick d'un. Grâce à la propriété onclick en html. On enregistre le nbclick dans une variable appelée result
-	var result = nbclick++;
-	}
+	var result = nbclick++;	
+//On retourne result
+	return result;
+}
