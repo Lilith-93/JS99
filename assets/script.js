3a918e0d3242de5667cbed20b5d439368dcbdaaa
Lilith-93 <deborah.delcourt@free.fr>
2017-07-24 23:16:34 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//Fonction pour compter le nombre de clique sur nbclick
+function count(){
+//On crée la variable nbclick
+	var nbclick = 0;
+//A chaque click, on augmente nbclick d'un. Grâce à la propriété onclick en html. On enregistre le nbclick dans une variable appelée result
+	var result = nbclick++;
+	}
